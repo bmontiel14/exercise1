c762a15385be8f490af61b97e2ac5eb897186557
bmontiel14 <bryan.juice.montiel@gmail.com>
2021-01-27 23:52:19 -0500

    initial commit

---
+++ src/com/company/montiel/Main.java
@@ -0,0 +1,20 @@
+package com.company.montiel;
+
+public class Main {
+
+    public static void main(String[] args) {
+    String city = "Columbus";
+    int zip = 43123;
+    int highTemperatures[] = {52,66,45,37,50};
+    int total = 52+66+45+37+50;
+    int days = 5;
+    double average = total/days;
+        System.out.println("City: " +city+ " Zip Code: " +zip+  " Average High Temperature: " +average);
+
+
+
+
+
+
+    }
+}
